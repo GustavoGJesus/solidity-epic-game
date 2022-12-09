4e54fb24a0d1717bff10ef56fcc25d89307e3831
GustavoGJesus <ggomesdejesus01@gmail.com>
2022-12-08 23:35:06 -0300

    feat: create contract for create characters

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,36 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("This is the contract of my game, let's go man!!");
+  struct CharacterAttributes {
+    uint characterIndex;
+    string name;
+    string imageURI;
+    uint hp;
+    uint maxHp;
+    uint attackDamage;
+  }
+
+  CharacterAttributes[] defaultCharacters;
+
+  constructor(
+    string[] memory characterNames,
+    string[] memory characterImageURIs,
+    uint[] memory characterHp,
+    uint[] memory characterAttackDmg
+  )
+  {
+    for(uint i = 0; i < characterNames.length; i += 1) {
+      defaultCharacters.push(CharacterAttributes({
+        characterIndex: i,
+        name: characterNames[i],
+        imageURI: characterImageURIs[i],
+        hp: characterHp[i],
+        maxHp: characterHp[i],
+        attackDamage: characterAttackDmg[i]
+       }));
+
+       CharacterAttributes memory c = defaultCharacters[i];
+       console.log("Personage inicialized: %s with %s of HP, img %s", c.name, c.hp, c.imageURI);
     }
+  }
 }
