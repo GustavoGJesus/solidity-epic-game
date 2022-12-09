c9949dab57f4a53f12739b3d026adedbd140822b
GustavoGJesus <ggomesdejesus01@gmail.com>
2022-12-08 23:35:22 -0300

    feat: save data in contract

--- scripts/run.ts
+++ scripts/run.ts
@@ -2,9 +2,18 @@ const hre = require("hardhat");
 
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["ADA Cardano", "Shiba", "Solana"],
+    [
+      "https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/cardano_ada-512.png",
+      "https://cryptologos.cc/logos/shiba-inu-shib-logo.png",
+      "https://seeklogo.com/images/S/solana-sol-logo-12828AD23D-seeklogo.com.png",
+    ],
+    [300, 100, 200],
+    [100, 50, 25]
+  );
   await gameContract.deployed();
-  console.log("Contrato implantado no endereço:", gameContract.address);
+  console.log("Contract deployed in address:", gameContract.address);
 };
 
 const runMain = async () => {
